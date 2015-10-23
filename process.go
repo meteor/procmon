3bb3e9d9071a8ed07a2a3d233509fe2165e74229
Graham Hughes <ghughes@meteor.com>
2015-10-23 02:21:32 -0700

    Add initial stab at parsing data

---
+++ process.go
@@ -0,0 +1,224 @@
+// Package procmon is a simple library for recording samples of a
+// process and sending the results to datadog.
+package procmon
+
+import (
+	"bufio"
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"io"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// Measure is a point in time measure of a process's resource consumption.
+type Measure struct {
+	// User is the % of usermode CPU time used
+	User float64
+	// System is the % of kernel mode CPU time used
+	System float64
+	// Memory is the amount of memory used
+	Memory uint64
+}
+
+// Point in time measure of a process's state
+type point struct {
+	user   uint64
+	system uint64
+}
+
+// Monitor represents a continuous monitoring of a given Linux
+// process.
+type Monitor struct {
+	Output  chan<- Measure
+	ticker  *time.Ticker
+	process int
+	done    chan bool
+	stats   point
+	total   point
+}
+
+// New creates a new monitor and starts it.
+func New(out chan<- Measure, process int) (*Monitor, error) {
+	m := new(Monitor)
+	m.ticker = time.NewTicker(5 * time.Second)
+	m.done = make(chan bool, 1)
+	m.process = process
+	m.Output = out
+	if err := m.preflight(); err != nil {
+		return nil, err
+	}
+	go m.Monitor()
+	return m, nil
+}
+
+func (m *Monitor) preflight() error {
+	_, err := m.fetchProcessUsage()
+	if err != nil {
+		return err
+	}
+	_, err = m.fetchTotalUsage()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func parseProcStat(in io.Reader) (point, error) {
+	// per proc(5) we are after fields cutime and cstime, numbers 16
+	// and 17.
+	contents, err := ioutil.ReadAll(in)
+	if err != nil {
+		return point{}, err
+	}
+	fields := strings.Split(string(contents), " ")
+	if len(fields) < 17 {
+		return point{}, fmt.Errorf("Not enough fields")
+	}
+	cutime, err := strconv.ParseUint(fields[15], 10, 64)
+	if err != nil {
+		return point{}, err
+	}
+	cstime, err := strconv.ParseUint(fields[16], 10, 64)
+	if err != nil {
+		return point{}, err
+	}
+	return point{cutime, cstime}, nil
+}
+
+func parseMemStat(in io.Reader) (uint64, error) {
+	// per proc(5) we are after field rss, number 2.
+	contents, err := ioutil.ReadAll(in)
+	if err != nil {
+		return 0, err
+	}
+	fields := strings.Split(string(contents), " ")
+	if len(fields) < 2 {
+		return 0, fmt.Errorf("Not enough fields")
+	}
+	rss, err := strconv.ParseUint(fields[1], 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	return rss, nil
+}
+
+func parseGlobalStat(in io.Reader) (point, error) {
+	// again per proc(5) we are after the user and system fields from
+	// the cpu line, which are fields 1 and 3.  This is complicated
+	// because /proc/stat gives lots of information we don't want or
+	// need.
+	s := bufio.NewScanner(in)
+	for s.Scan() {
+		if strings.HasPrefix(s.Text(), "cpu ") {
+			fs := bufio.NewScanner(strings.NewReader(s.Text()))
+			fs.Split(bufio.ScanWords)
+			for i := 0; i < 2; i++ {
+				if !fs.Scan() {
+					return point{}, fmt.Errorf("cpu line ended before user data seen: %q", s.Text())
+				}
+			}
+			utime, err := strconv.ParseUint(fs.Text(), 10, 64)
+			if err != nil {
+				return point{}, err
+			}
+			for i := 0; i < 2; i++ {
+				if !fs.Scan() {
+					return point{}, fmt.Errorf("cpu line ended before system data seen: %q", s.Text())
+				}
+			}
+			stime, err := strconv.ParseUint(fs.Text(), 10, 64)
+			if err != nil {
+				return point{}, err
+			}
+			return point{utime, stime}, nil
+		}
+	}
+	return point{}, fmt.Errorf("No line starting with 'cpu' seen")
+}
+
+func (m *Monitor) fetchProcessUsage() (point, error) {
+	file, err := os.Open(fmt.Sprintf("/proc/%d/stat", m.process))
+	if err != nil {
+		return point{}, err
+	}
+	defer file.Close()
+	return parseProcStat(file)
+}
+
+func (m *Monitor) fetchProcessMemory() (uint64, error) {
+	file, err := os.Open(fmt.Sprintf("/proc/%d/statm", m.process))
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+	return parseMemStat(file)
+}
+
+func (m *Monitor) fetchTotalUsage() (point, error) {
+	file, err := os.Open("/proc/stat")
+	if err != nil {
+		return point{}, err
+	}
+	defer file.Close()
+	return parseGlobalStat(file)
+}
+
+// Monitor runs in a background goroutine that can be halted with Stop
+// and monitors process metrics, submitting results to datadog every 5
+// seconds.
+func (m *Monitor) Monitor() {
+	var err error
+	m.stats, err = m.fetchProcessUsage()
+	if err != nil {
+		log.WithField("process", m.process).WithError(err).
+			Error("couldn't read process stats")
+	}
+	m.total, err = m.fetchTotalUsage()
+	if err != nil {
+		log.WithField("process", m.process).WithError(err).
+			Error("couldn't read total CPU stats")
+	}
+	for {
+		select {
+		case <-m.ticker.C:
+			newtarget, err := m.fetchProcessUsage()
+			if err != nil {
+				log.WithField("process", m.process).WithError(err).
+					Error("couldn't read process stats")
+			}
+			memory, err := m.fetchProcessMemory()
+			if err != nil {
+				log.WithField("process", m.process).WithError(err).
+					Error("couldn't read process stats")
+			}
+			newtotal, err := m.fetchTotalUsage()
+			if err != nil {
+				log.WithField("process", m.process).WithError(err).
+					Error("couldn't read total CPU stats")
+			}
+			userTotalDiff := newtotal.user - m.total.user
+			sysTotalDiff := newtotal.system - m.total.system
+
+			userPerc := 100.0 * float64(newtarget.user-m.stats.user) /
+				float64(userTotalDiff)
+			sysPerc := 100.0 * float64(newtarget.system-m.stats.system) /
+				float64(sysTotalDiff)
+
+			m.Output <- Measure{userPerc, sysPerc, memory}
+			m.stats = newtarget
+			m.total = newtotal
+		case <-m.done:
+			return
+		}
+	}
+}
+
+// Stop halts the background monitoring task.
+func (m *Monitor) Stop() {
+	m.done <- true
+	m.ticker.Stop()
+}
---
+++ process_io_test.go
@@ -0,0 +1,106 @@
+package procmon
+
+import (
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func TestSimpleProcess(t *testing.T) {
+	point, err := parseProcStat(strings.NewReader(`1735 (sh) S 1734 1735 1735 34816 2679 4218880 655 3141 0 0 0 0 0 1 20 0 1 0 182865 12144640 534 18446744073709551615 4194304 4729572 140730058798800 140730058796792 139881841869352 0 0 2637828 2 0 0 0 17 0 0 0 0 0 0 6826728 6830659 16642048 140730058800890 140730058800894 140730058800894 140730058801136 0`))
+	if assert.NoError(t, err) {
+		assert.Equal(t, point.user, uint64(0))
+		assert.Equal(t, point.system, uint64(1))
+	}
+	point, err = parseProcStat(strings.NewReader(`1735 (sh) S 1734 1735 1735 34816 2679 4218880 655 3141 0 0 0 0 152 189 20 0 1 0 182865 12144640 534 18446744073709551615 4194304 4729572 140730058798800 140730058796792 139881841869352 0 0 2637828 2 0 0 0 17 0 0 0 0 0 0 6826728 6830659 16642048 140730058800890 140730058800894 140730058800894 140730058801136 0`))
+	if assert.NoError(t, err) {
+		assert.Equal(t, point.user, uint64(152))
+		assert.Equal(t, point.system, uint64(189))
+	}
+}
+
+func TestBrokenProcess(t *testing.T) {
+	_, err := parseProcStat(strings.NewReader(`1735 (sh) S 1734 1735 1735`))
+	assert.Error(t, err)
+	_, err = parseProcStat(strings.NewReader(``))
+	assert.Error(t, err)
+	_, err = parseProcStat(strings.NewReader(`1735 (sh) S 1734 1735 1735 34816 2679 4218880 655 3141 0 0 0 0 frob 1 20 0 1 0 182865 12144640 534 18446744073709551615 4194304 4729572 140730058798800 140730058796792 139881841869352 0 0 2637828 2 0 0 0 17 0 0 0 0 0 0 6826728 6830659 16642048 140730058800890 140730058800894 140730058800894 140730058801136 0`))
+	assert.Error(t, err)
+	_, err = parseProcStat(strings.NewReader(`1735 (sh) S 1734 1735 1735 34816 2679 4218880 655 3141 0 0 0 0 0 botz 20 0 1 0 182865 12144640 534 18446744073709551615 4194304 4729572 140730058798800 140730058796792 139881841869352 0 0 2637828 2 0 0 0 17 0 0 0 0 0 0 6826728 6830659 16642048 140730058800890 140730058800894 140730058800894 140730058801136 0`))
+	assert.Error(t, err)
+	_, err = parseProcStat(strings.NewReader(`1735 (sh) S 1734 1735 1735 34816 2679 4218880 655 3141 0 0 0 0 -1 1 20 0 1 0 182865 12144640 534 18446744073709551615 4194304 4729572 140730058798800 140730058796792 139881841869352 0 0 2637828 2 0 0 0 17 0 0 0 0 0 0 6826728 6830659 16642048 140730058800890 140730058800894 140730058800894 140730058801136 0`))
+	assert.Error(t, err)
+}
+
+func TestSimpleMemory(t *testing.T) {
+	point, err := parseMemStat(strings.NewReader(`2965 534 485 131 0 129 0`))
+	if assert.NoError(t, err) {
+		assert.Equal(t, point, uint64(534))
+	}
+}
+
+func TestBrokenMemory(t *testing.T) {
+	_, err := parseMemStat(strings.NewReader(`1735`))
+	assert.Error(t, err)
+	_, err = parseMemStat(strings.NewReader(``))
+	assert.Error(t, err)
+	_, err = parseMemStat(strings.NewReader(`1735 (sh) S`))
+	assert.Error(t, err)
+	_, err = parseMemStat(strings.NewReader(`1735 -24`))
+	assert.Error(t, err)
+}
+
+func TestSimpleEverything(t *testing.T) {
+	input := `cpu  2019 0 929 687424 84 1 34 0 0 0
+cpu0 2019 0 929 687424 84 1 34 0 0 0
+intr 166310 14 10 0 0 0 0 0 0 1 0 0 0 156 0 0 0 7407 0 0 8335 10262 6589 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ctxt 296117
+btime 1445583144
+processes 2833
+procs_running 2
+procs_blocked 0
+softirq 134624 0 68894 2777 14447 5489 0 18 0 29 42970`
+	point, err := parseGlobalStat(strings.NewReader(input))
+	if assert.NoError(t, err) {
+		assert.Equal(t, point.user, uint64(2019))
+		assert.Equal(t, point.system, uint64(929))
+	}
+	input = `cpu0 2019 0 929 687424 84 1 34 0 0 0
+intr 166310 14 10 0 0 0 0 0 0 1 0 0 0 156 0 0 0 7407 0 0 8335 10262 6589 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ctxt 296117
+btime 1445583144
+processes 2833
+cpu  2019 0 929 687424 84 1 34 0 0 0
+procs_running 2
+procs_blocked 0
+softirq 134624 0 68894 2777 14447 5489 0 18 0 29 42970`
+	point, err = parseGlobalStat(strings.NewReader(input))
+	if assert.NoError(t, err) {
+		assert.Equal(t, point.user, uint64(2019))
+		assert.Equal(t, point.system, uint64(929))
+	}
+	input = `cpu        2019 0 929 687424 84 1 34 0 0 0`
+	point, err = parseGlobalStat(strings.NewReader(input))
+	if assert.NoError(t, err) {
+		assert.Equal(t, point.user, uint64(2019))
+		assert.Equal(t, point.system, uint64(929))
+	}
+}
+
+func TestBrokenEverything(t *testing.T) {
+	_, err := parseGlobalStat(strings.NewReader(`cpu  2 3`))
+	assert.Error(t, err)
+	_, err = parseGlobalStat(strings.NewReader(``))
+	assert.Error(t, err)
+	_, err = parseGlobalStat(strings.NewReader(`cpu0 2019 0 929 687424 84 1 34 0 0 0
+intr 166310 14 10 0 0 0 0 0 0 1 0 0 0 156 0 0 0 7407 0 0 8335 10262 6589 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ctxt 296117
+btime 1445583144
+processes 2833
+procs_running 2
+procs_blocked 0
+softirq 134624 0 68894 2777 14447 5489 0 18 0 29 42970`))
+	assert.Error(t, err)
+	_, err = parseGlobalStat(strings.NewReader(`cpu      foo 4 21`))
+	assert.Error(t, err)
+}
