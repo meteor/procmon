13aa964d9d5da38cdaeec01fbe4e221ae5dc0f9b
Graham Hughes <ghughes@meteor.com>
2015-10-23 17:54:29 -0700

    Add simple tool for testing.

---
+++ client/reporter.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"github.com/meteor/procmon"
+	"strconv"
+)
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		fmt.Println("Input process is missing")
+	}
+
+	process, err := strconv.ParseInt(flag.Arg(0), 10, 32)
+	if err != nil {
+		log.WithField("input", flag.Arg(0)).WithError(err).Fatal("Couldn't parse input process")
+	}
+
+	output := make(chan procmon.Measure, 1)
+	monitor, err := procmon.New(output, int(process))
+	if err != nil {
+		log.WithError(err).Fatal("Couldn't parse input process")
+	}
+	defer monitor.Stop()
+
+outerloop:
+	for {
+		select {
+		case point, ok := <-output:
+			if !ok {
+				log.Warn("Not ok, breaking")
+				break outerloop
+			}
+			log.WithFields(log.Fields{
+				"user":   point.User,
+				"system": point.System,
+				"memory": point.Memory,
+			}).Info("Got point")
+		}
+	}
+}
