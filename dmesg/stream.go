a8ce7c97f57e809cdea1ea81cd9793c05584b45a
Graham Hughes <ghughes@meteor.com>
2015-11-06 15:48:49 -0800

    Add streaming interface.

---
+++ dmesg/stream.go
@@ -0,0 +1,57 @@
+package dmesg
+
+import "time"
+import "log"
+
+// Stream creates a goroutine that, every sampleTime ticks, will send
+// new dmesg messages to out.  It also listens on stop, in case you
+// need to abort the goroutine.  If there is an error setting up the
+// initial state, it is returned, but otherwise errors are logged and
+// otherwise ignored.
+func Stream(out chan<- *Message, stop <-chan bool, sampleTime time.Duration) error {
+	state, err := New()
+	if err != nil {
+		return err
+	}
+	go doStream(state, out, stop, sampleTime)
+	return nil
+}
+
+func doStream(state *State, out chan<- *Message, stop <-chan bool, sampleTime time.Duration) {
+	var lastMessage *Message
+	ticker := time.NewTicker(sampleTime)
+	defer ticker.Stop()
+	for _ = range ticker.C {
+		select {
+		case <-stop:
+			log.Println("Terminating as requested")
+			return
+		default:
+		}
+		messages, err := state.Messages()
+		if err == nil {
+			if lastMessage == nil {
+				for _, message := range messages {
+					lastMessage = message
+					out <- message
+				}
+			} else {
+				hasSeenLast := false
+				for _, message := range messages {
+					if message.Timestamp.After(lastMessage.Timestamp) {
+						log.Println("missed some, resuming where available")
+						hasSeenLast = true
+					} else if message == lastMessage {
+						hasSeenLast = true
+					}
+					if hasSeenLast {
+						lastMessage = message
+						out <- message
+					}
+				}
+			}
+		} else {
+			log.Println("Messages returned error; hoping it clears up")
+		}
+	}
+}
