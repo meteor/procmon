a133130e2205334060698fcba18d3b2f31b18911
Graham Hughes <ghughes@meteor.com>
2015-11-06 15:21:45 -0800

    Move system dependent stuff to correct place.

--- dmesg/dmesg.go
+++ dmesg/dmesg.go
@@ -1,85 +1,25 @@
 package dmesg
 
-import "bytes"
-import "fmt"
-import "io"
 import "time"
-import "strings"
-import "os"
-import "bufio"
 
+// Message encapsulates kernel ring buffer messages.  The timestamp is
+// absolute, not relative to boot time.
 type Message struct {
 	Level     int64
 	Timestamp time.Time
 	Message   string
 }
 
-func ParseMessages(bootTime time.Time, buffer []byte) ([]*Message, error) {
-	buf := bytes.NewBuffer(buffer)
-	result := make([]*Message, 0)
-	var lastMessage *Message
-	for {
-		rune, _, err := buf.ReadRune()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			return nil, err
-		}
-		if rune == ' ' {
-			// continuation line
-			line, err := buf.ReadString('\n')
-			if err != nil {
-				return nil, err
-			}
-			if lastMessage == nil {
-				return nil, fmt.Errorf("First line in ring buffer was a continuation line!")
-			}
-			lastMessage.Message += line[:len(line)-1]
-		} else {
-			if err := buf.UnreadRune(); err != nil {
-				return nil, err
-			}
-			line, err := buf.ReadString(']')
-			if err != nil {
-				return nil, err
-			}
-			var level, secs, nanosecs int64
-			matched, err := fmt.Sscanf(line, "<%d>[%d.%d]", &level, &secs, &nanosecs)
-			if err != nil {
-				return nil, err
-			}
-			if matched != 3 {
-				return nil, fmt.Errorf("Couldn't parse line for some reason")
-			}
-			message, err := buf.ReadString('\n')
-			if err != nil && err != io.EOF {
-				return nil, err
-			}
-			adjustedTime := bootTime.Add(time.Second*time.Duration(secs) + time.Nanosecond*time.Duration(nanosecs))
-			result = append(result, &Message{
-				level,
-				adjustedTime,
-				strings.TrimSuffix(message, "\n"),
-			})
-		}
-	}
-	return result, nil
-}
-
-func Uptime() (time.Time, error) {
-	file, err := os.Open("/proc/stat")
+// Messages retrieves all kernel ring buffer messages and returns
+// them, or a reason why it could not.
+func Messages() ([]*Message, error) {
+	buffer, err := Current()
 	if err != nil {
-		return time.Unix(0, 0), err
+		return nil, err
 	}
-	defer file.Close()
-	s := bufio.NewScanner(file)
-	for s.Scan() {
-		var btime int64
-		matched, err := fmt.Sscanf(s.Text(), "btime %d\n", &btime)
-		if err == nil && matched == 1 {
-			// aha, that's the line.
-			return time.Unix(btime, 0), nil
-		}
+	messages, err := ParseMessages(buffer)
+	if err != nil {
+		return nil, err
 	}
-	return time.Unix(0, 0), fmt.Errorf("Did not find btime declaration in /proc/stat")
+	return messages, nil
 }
--- dmesg/sysctl.go
+++ dmesg/sysctl.go
@@ -2,7 +2,14 @@
 
 package dmesg
 
+import "bufio"
+import "os"
+import "bytes"
+import "io"
 import "golang.org/x/sys/unix"
+import "strings"
+import "time"
+import "fmt"
 
 // dunno why these aren't exposed by the sys module
 const syslogActionSizeBuffer = 10
@@ -20,4 +27,81 @@ func Current() ([]byte, error) {
 		return nil, err
 	}
 	return buffer[:bytesread], nil
+}
+
+func uptime() (time.Time, error) {
+	file, err := os.Open("/proc/stat")
+	if err != nil {
+		return time.Unix(0, 0), err
+	}
+	defer file.Close()
+	s := bufio.NewScanner(file)
+	for s.Scan() {
+		var btime int64
+		matched, err := fmt.Sscanf(s.Text(), "btime %d\n", &btime)
+		if err == nil && matched == 1 {
+			// aha, that's the line.
+			return time.Unix(btime, 0), nil
+		}
+	}
+	return time.Unix(0, 0), fmt.Errorf("Did not find btime declaration in /proc/stat")
+}
+
+// ParseMessages reads dmesg type messages out of buffer.  To do so it
+// must read the system boot time out of /proc/stat, because dmesg
+// timestamps are relative to when the system booted.
+func ParseMessages(buffer []byte) ([]*Message, error) {
+	bootTime, err := uptime()
+	if err != nil {
+		return nil, err
+	}
+	buf := bytes.NewBuffer(buffer)
+	var result []*Message
+	var lastMessage *Message
+	for {
+		rune, _, err := buf.ReadRune()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+		if rune == ' ' {
+			// continuation line
+			line, err := buf.ReadString('\n')
+			if err != nil {
+				return nil, err
+			}
+			if lastMessage == nil {
+				return nil, fmt.Errorf("First line in ring buffer was a continuation line!")
+			}
+			lastMessage.Message += line[:len(line)-1]
+		} else {
+			if err := buf.UnreadRune(); err != nil {
+				return nil, err
+			}
+			line, err := buf.ReadString(']')
+			if err != nil {
+				return nil, err
+			}
+			var level, secs, nanosecs int64
+			matched, err := fmt.Sscanf(line, "<%d>[%d.%d]", &level, &secs, &nanosecs)
+			if err != nil {
+				return nil, err
+			}
+			if matched != 3 {
+				return nil, fmt.Errorf("Couldn't parse line %q for some reason", line)
+			}
+			message, err := buf.ReadString('\n')
+			if err != nil && err != io.EOF {
+				return nil, err
+			}
+			adjustedTime := time.Unix(secs, nanosecs).Add(time.Second * time.Duration(bootTime.Unix()))
+			result = append(result, &Message{
+				level,
+				adjustedTime,
+				strings.TrimSuffix(message[1:], "\n"),
+			})
+		}
+	}
+	return result, nil
 }
